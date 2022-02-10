28530b9b8d3319c3a101244f9bd93df9041be03d
AndreaScacchi <andreascacchi10@gmail.com>
2022-02-10 12:31:43 +0100

    wrap every letter in a span

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
 /* animate the title */
 // Wrapp every letter in a span
 var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\s/g, "<span class='letter'>$&</span>");
