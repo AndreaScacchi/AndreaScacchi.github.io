360ccfe07d27cca83438e888aecca44296116d64
AndreaScacchi <andreascacchi10@gmail.com>
2022-02-23 12:53:48 +0100

    API KEY created and added to implement the functionality of the currency converter

--- script/script.js
+++ script/script.js
@@ -42,7 +42,7 @@ for(let i = 0; i < dropList.length; i++) {
     for(let currency_code in country_list) {
         let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "NPR" ? "selected" : "";
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-        dropList[i].insertAdjacentHTML("before", optionTag);
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
     dropList[i].addEventListener("change", e => {
         loadFlag(e.target);
@@ -59,7 +59,7 @@ function loadFlag(element) {
     }
 }
 
-window.add.addEventListener("load", () => {
+window.addEventListener("load", () => {
     getExchangeRate();
 });
 
@@ -89,7 +89,7 @@ function getExchangeRate() {
         amountVal = 1;
     }
     exchangeRateTxt.innerText = "Getting exchange rate...";
-    let url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/${fromCurrency.value}`;
+    let url = `https://v6.exchangerate-api.com/v6/9777ffd1356b7342428d239a/latest/${fromCurrency.value}`;
     fetch(url).then(response => response.json()).then(result => {
         let exchangeRate = result.conversion_rates[toCurrency.value];
         let totalExRate = (amountVal * exchangeRate).toFixed(2);
