2b8bbe354b1c4b0774a41086ad1f04779330c3b8
AndreaScacchi <andreascacchi10@gmail.com>
2022-03-01 19:11:23 +0100

    progress bar modified when resize the page

--- script/script.js
+++ script/script.js
@@ -142,5 +142,28 @@ $(window).on("load", function () {
 }); 
 
 // Input hamburger menu
-const input = document.getElementById('checkbox_toggle');
+/*const input = document.getElementById('checkbox_toggle').checked;
 const progress_bar = document.querySelector('.progress_container');
+
+function checkInput() {
+    input.addEventListener('click', () => {
+        if (input === true) {
+            progress_bar.style.display = "none";
+                } else {
+            progress_bar.style.display = "block";
+        }
+    })
+}
+
+checkInput();*/
+
+const input = document.getElementById("checkbox_toggle");
+const progress_bar = document.querySelector(".progress_container");
+
+input.addEventListener("change", () => {
+    if(input.checked) {
+        progress_bar.style.display = "none";
+    } else {
+        progress_bar.style.display = "block";
+    }
+})
