6d8978a68a87f7d13eb69eb1571b08d4af7061b5
AndreaScacchi <andreascacchi10@gmail.com>
2022-03-05 20:54:56 +0100

    file updated

--- script/script.js
+++ script/script.js
@@ -1,42 +1,4 @@
 ////////////////////////////////
-/* ANIMATIONS */
-///////////////////////////////
-
-/* animate the title */
-// Wrapp every letter in a span
-var textWrapper = document.querySelector('.anime');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// Set Anime library to perform the animation on title
-anime.timeline({loop: true})
-    .add({
-        targets: '.anime .letter',
-        scale: [4,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: (el, i) => 70*i
-    }).add({
-        targets: '.anime',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 2000
-    });
-
-// Initialize AOS library to create dynamic sections
-AOS.init({
-    startEvent: "DOMContentLoaded",
-    duration: 2000,
-    delay: 0,
-    easing: "ease",
-    once: false,
-    mirror: false,
-});
-
-
-////////////////////////////////
 /* Loading spinner using jQuery */
 ///////////////////////////////
 /*$(window).on("load", function () {
@@ -145,7 +107,7 @@ window.addEventListener("load", () => {
     getExchangeRate();
 });
 
-getButton.addEventListener("click", e => {
+getButton.addEventListener("click", (e) => {
     e.preventDefault();
     getExchangeRate();
 });
@@ -180,3 +142,40 @@ function getExchangeRate() {
         exchangeRateTxt.innerText = "Something went wrong";
     });
 }
+
+////////////////////////////////
+/* ANIMATIONS */
+///////////////////////////////
+
+/* animate the title */
+// Wrapp every letter in a span
+var textWrapper = document.querySelector('.anime');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// Set Anime library to perform the animation on title
+anime.timeline({loop: true})
+    .add({
+        targets: '.anime .letter',
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70*i
+    }).add({
+        targets: '.anime',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 2000
+    });
+
+// Initialize AOS library to create dynamic sections
+AOS.init({
+    startEvent: "DOMContentLoaded",
+    duration: 2000,
+    delay: 0,
+    easing: "ease",
+    once: false,
+    mirror: false,
+});
