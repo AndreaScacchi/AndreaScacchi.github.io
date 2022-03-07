7efcdd37d5ba6914fe530dd0ee60c63b75c589b7
AndreaScacchi <andreascacchi10@gmail.com>
2022-03-07 16:47:20 +0100

    new comment added

--- script/gallery.js
+++ script/gallery.js
@@ -2,7 +2,9 @@
 /* GALLERY API */
 ///////////////////////////////
 
-/* Get Rome photos */
+/* Global Variables */
+
+/* Get Rome photos from Unsplash API */
 let count = 5;
 const apiKey = "ZK6O_Hy2nnJUGWGuQ7oEpKcvWi51Q1RNPbBm5VMp4k0";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=rome`;
