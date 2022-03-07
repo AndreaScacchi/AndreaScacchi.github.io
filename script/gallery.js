44ac7ab1d1a9923e9b8c1ee81da1e65a84ea5596
AndreaScacchi <andreascacchi10@gmail.com>
2022-03-07 17:46:35 +0100

    gallery implemented

--- script/gallery.js
+++ script/gallery.js
@@ -79,6 +79,7 @@ async function getPhotos() {
 window.addEventListener('scroll', () => {
     if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
         ready = false;
+        getPhotos();
     }
 })
 
