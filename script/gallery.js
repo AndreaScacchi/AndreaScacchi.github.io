c453e2791bd03228dbaeac0e42f449339ed6cbf2
AndreaScacchi <andreascacchi10@gmail.com>
2022-03-12 12:24:38 +0100

    small changes

--- script/gallery.js
+++ script/gallery.js
@@ -3,26 +3,25 @@
 ///////////////////////////////
 
 /* Global Variables */
-const imageContainer = document.getElementById('images_container');
-const loader = document.getElementById('loader_img');
+const imageContainer = document.getElementById("images_container");
+const loader = document.getElementById("loader_img");
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
 const sectionGallery = document.getElementById("section_loader");
 
-function galleryLoading() {
-    if (navigator.userAgent.toLowerCase().match(/android/)) {
-        sectionGallery.style.backgroundAttachment = "fixed";
-    } else if (
-        navigator.userAgent.toLowerCase().match(/iphone/) ||
-        navigator.userAgent.toLowerCase().match(/ipad/)
-    ) {
-        sectionGallery.style.backgroundColor = "#776640";
-    }
+/*function galleryLoading() {
+  if (navigator.userAgent.toLowerCase().match(/android/)) {
+    sectionGallery.style.backgroundAttachment = "fixed";
+  } else if (
+    navigator.userAgent.toLowerCase().match(/iphone/) ||
+    navigator.userAgent.toLowerCase().match(/ipad/)
+  ) {
+    sectionGallery.style.backgroundColor = "#776640";
+  }
 }
-galleryLoading();
-
+galleryLoading();*/
 
 /* Get Rome photos from Unsplash API */
 let count = 10;
@@ -31,72 +30,75 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 // Check if all iamges were loaded
 function imageLoaded() {
-    imagesLoaded++;
-    console.log(imagesLoaded);
-    if(imagesLoaded === totalImages) {
-        ready = true;
-        loader.hidden = true;
-        count = 30;
-        console.log("ready = ", ready);
-    }
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    count = 30;
+    console.log("ready = ", ready);
+  }
 }
 
 // Helper functio to quickly assign elements with IDs to constants
 function setAttributes(element, attributes) {
-    for(const key in attributes) {
-        element.setAttribute(key, attributes[key]);
-    }
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
 
 // Create elements for links & photos, add to DOM
 function displayPhotos() {
-    imagesLoaded = 0;
-    totalImages = photosArray.length;
-    console.log("Total Images", totalImages);
-    photosArray.forEach((photo) => {
-        // Create <a> to link to Unsplash
-        const item = document.createElement("a");
-        setAttributes(item, {
-            href: photo.links.html,
-            target: "_blank",
-        });
-        // Create <img> for photo
-        const img = document.createElement("img");
-        setAttributes(img, {
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        });
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("Total Images", totalImages);
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // Create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
-        // Event listener, check when each is finished loading
-        img.addEventListener("load", imageLoaded);
+    // Event listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
 
-        // Put <img> inside <a>, then puth both inside imageContainer element
-        item.appendChild(img);
-        imageContainer.appendChild(item);
-    })
+    // Put <img> inside <a>, then puth both inside imageContainer element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
 }
 
 // Get photos
 async function getPhotos() {
-    try {
-        const response = await fetch(apiUrl);
-        photosArray = await response.json();
-        displayPhotos();
-    } catch (err) {
-        if(err instanceof Error) {
-            console.log(`Error: ${err.message}`);
-        }
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(`Error: ${err.message}`);
     }
+  }
 }
 
 // Check to see if scrolling near bottom of page, load more photos
-window.addEventListener('scroll', () => {
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
-        ready = false;
-        getPhotos();
-    }
-})
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // Function call
 getPhotos();
--- script/script.js
+++ script/script.js
@@ -9,52 +9,54 @@
 
 /* Second method to implemented the spinner before the page is loaded */
 document.onreadystatechange = function () {
-    if(document.readyState == "complete") {
-        setTimeout(function() {
-            document.getElementById('loading').style.display = "none";
-        }, 2000);
-    }
-}
-
+  if (document.readyState == "complete") {
+    setTimeout(function () {
+      document.getElementById("loading").style.display = "none";
+    }, 2000);
+  }
+};
 
 ////////////////////////////////
 /* Set date for copyright */
 ///////////////////////////////
-const date = document.getElementById('date');
+const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
-
 
 ////////////////////////////////
 /* HEADER */
 ///////////////////////////////
 
 /* Fixed Navbar */
-const navbar = document.getElementById('navbar_id');
-window.addEventListener('scroll', function() {
-    if(window.scrollY > 1) {
-        navbar.classList.add('navbar_fixed');
-    } else {
-        navbar.classList.remove('navbar_fixed');
-    }
-})
+const navbar = document.getElementById("navbar_id");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 1) {
+    navbar.classList.add("navbar_fixed");
+  } else {
+    navbar.classList.remove("navbar_fixed");
+  }
+});
 
 /* Scroll into contact section */
-document.querySelector('.contact_link').addEventListener('click', function(e) {
-    e.preventDefault();
+document.querySelector(".contact_link").addEventListener("click", function (e) {
+  e.preventDefault();
 
-    if(e.target.classList.contains("contact_link")) {
-        const id = e.target.getAttribute("href");
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-    }
+  if (e.target.classList.contains("contact_link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
 
 /* Scroll indicator */
-window.onscroll = function() {scrollIndicator()};
+window.onscroll = function () {
+  scrollIndicator();
+};
 function scrollIndicator() {
-    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = (winScroll / height) * 100;
-    document.getElementById('progress_bar').style.width = scrolled + '%';
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("progress_bar").style.width = scrolled + "%";
 }
 
 // Input hamburger menu
@@ -62,109 +64,123 @@ const input = document.getElementById("checkbox_toggle");
 const progress_bar = document.querySelector(".progress_container");
 
 input.addEventListener("change", () => {
-    if(input.checked) {
-        progress_bar.style.display = "none";
-    } else {
-        progress_bar.style.display = "block";
-    }
-})
-
-/* Scroll down button into header using jQuery*/
-const scrollDown = document.querySelector('.scroll_down');
-const section1 = document.getElementById('section_1');
-const section4 = document.getElementById("section_4");
-
-
-$(function() {
-    $('.scroll_down').click(function() {
-        $('html, body').animate({ scrollTop: $('#section_1').offset().top }, 'slow');
-        return false;
-    });
+  if (input.checked) {
+    progress_bar.style.display = "none";
+  } else {
+    progress_bar.style.display = "block";
+  }
 });
 
+/* Scroll down button into header using jQuery*/
+const scrollDown = document.querySelector(".scroll_down");
+const section1 = document.getElementById("section_1");
+const section4 = document.getElementById("section_4");
+
+$(function () {
+  $(".scroll_down").click(function () {
+    $("html, body").animate(
+      { scrollTop: $("#section_1").offset().top },
+      "slow"
+    );
+    return false;
+  });
+});
 
 function loadDeviceSpecificBackground() {
-    if(navigator.userAgent.toLowerCase().match(/android/)) {
-        section1.style.backgroundAttachment = "fixed";
-        section4.style.backgroundAttachment = "fixed";
-    } else if(navigator.userAgent.toLowerCase().match(/iphone/) || navigator.userAgent.toLowerCase().match(/ipad/)) {
-        section1.style.backgroundAttachment = "scroll";
-        section4.style.backgroundAttachment = "scroll";
-    }
+  if (navigator.userAgent.toLowerCase().match(/android/)) {
+    section1.style.backgroundAttachment = "fixed";
+    section4.style.backgroundAttachment = "fixed";
+  } else if (
+    navigator.userAgent.toLowerCase().match(/iphone/) ||
+    navigator.userAgent.toLowerCase().match(/ipad/)
+  ) {
+    section1.style.backgroundAttachment = "scroll";
+    section4.style.backgroundAttachment = "scroll";
+  }
 }
 loadDeviceSpecificBackground();
-
-
 
 ////////////////////////////////
 /* SECTION_4 */
 ///////////////////////////////
 
 /* Currency converter */
-const dropList = document.querySelectorAll('form select');
-const fromCurrency = document.querySelector('.from select');
-const toCurrency = document.querySelector('.to select');
-const getButton = document.querySelector('form button');
+const dropList = document.querySelectorAll("form select");
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
+const getButton = document.querySelector("form button");
 
 /* Select country from the drop list */
-for(let i = 0; i < dropList.length; i++) {
-    for(let currency_code in country_list) {
-        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "EUR" ? "selected" : "";
-        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-        dropList[i].insertAdjacentHTML("beforeend", optionTag);
-    }
-    dropList[i].addEventListener("change", e => {
-        loadFlag(e.target);
-    });
+for (let i = 0; i < dropList.length; i++) {
+  for (let currency_code in country_list) {
+    let selected =
+      i == 0
+        ? currency_code == "USD"
+          ? "selected"
+          : ""
+        : currency_code == "EUR"
+        ? "selected"
+        : "";
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+  dropList[i].addEventListener("change", (e) => {
+    loadFlag(e.target);
+  });
 }
 
 /* Function to load the flag country */
 function loadFlag(element) {
-    for(let code in country_list) {
-        if(code == element.value) {
-            let imgTag = element.parentElement.querySelector('img');
-            imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
-        }
+  for (let code in country_list) {
+    if (code == element.value) {
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://flagcdn.com/48x36/${country_list[
+        code
+      ].toLowerCase()}.png`;
     }
+  }
 }
 
 window.addEventListener("load", () => {
-    getExchangeRate();
+  getExchangeRate();
 });
 
 getButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    getExchangeRate();
+  e.preventDefault();
+  getExchangeRate();
 });
 
 /* Implemented the exchange icon */
-const exchangeIcon = document.querySelector('form .icon');
+const exchangeIcon = document.querySelector("form .icon");
 exchangeIcon.addEventListener("click", () => {
-    let tempCode = fromCurrency.value;
-    fromCurrency.value = toCurrency.value;
-    toCurrency.value = tempCode;
-    loadFlag(fromCurrency);
-    loadFlag(toCurrency);
-    getExchangeRate();
-})
+  let tempCode = fromCurrency.value;
+  fromCurrency.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  loadFlag(fromCurrency);
+  loadFlag(toCurrency);
+  getExchangeRate();
+});
 
 /* Function to implement the exchange rate */
 function getExchangeRate() {
-    const amount = document.querySelector('form input');
-    const exchangeRateTxt = document.querySelector('form .exchange-rate');
-    let amountVal = amount.value;
-    if(amountVal == "" || amountVal == "0") {
-        amount.value = "1";
-        amountVal = 1;
-    }
-    exchangeRateTxt.innerText = "Getting exchange rate...";
-    let url = `https://v6.exchangerate-api.com/v6/9777ffd1356b7342428d239a/latest/${fromCurrency.value}`;
-    fetch(url).then(response => response.json()).then(result => {
-        let exchangeRate = result.conversion_rates[toCurrency.value];
-        let totalExRate = (amountVal * exchangeRate).toFixed(2);
-        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
-    }).catch(() => {
-        exchangeRateTxt.innerText = "Something went wrong";
+  const amount = document.querySelector("form input");
+  const exchangeRateTxt = document.querySelector("form .exchange-rate");
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+  let url = `https://v6.exchangerate-api.com/v6/9777ffd1356b7342428d239a/latest/${fromCurrency.value}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let exchangeRate = result.conversion_rates[toCurrency.value];
+      let totalExRate = (amountVal * exchangeRate).toFixed(2);
+      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    })
+    .catch(() => {
+      exchangeRateTxt.innerText = "Something went wrong";
     });
 }
 
@@ -174,33 +190,38 @@ function getExchangeRate() {
 
 /* animate the title */
 // Wrapp every letter in a span
-var textWrapper = document.querySelector('.anime');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var textWrapper = document.querySelector(".anime");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
 
 // Set Anime library to perform the animation on title
-anime.timeline({loop: true})
-    .add({
-        targets: '.anime .letter',
-        scale: [4,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: (el, i) => 70*i
-    }).add({
-        targets: '.anime',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 2000
-    });
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".anime .letter",
+    scale: [4, 1],
+    opacity: [0, 1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70 * i,
+  })
+  .add({
+    targets: ".anime",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 2000,
+  });
 
 // Initialize AOS library to create dynamic sections
 AOS.init({
-    startEvent: "DOMContentLoaded",
-    duration: 2000,
-    delay: 0,
-    easing: "ease",
-    once: false,
-    mirror: false,
+  startEvent: "DOMContentLoaded",
+  duration: 2000,
+  delay: 0,
+  easing: "ease",
+  once: false,
+  mirror: false,
 });
